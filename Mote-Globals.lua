ef5a7545d1c82c7ea6ea27c23e7bd5400cbb2906
eisen203 <60910090+eisen203@users.noreply.github.com>
2024-01-18 20:28:54 -0600

    Update Mote-Globals.lua

--- Mote-Globals.lua
+++ Mote-Globals.lua
@@ -517,39 +517,6 @@ function get_weather_intensity()
 end
 
 
---Function Kohh's Run spell thing
-fixed_pos = ''
-fixed_ts = os.time()
-
-local no_interruptions = true
-
-windower.raw_register_event('outgoing chunk',function(id,original,modified,injected,blocked)
-    if no_interruptions and (not blocked) then
-        if id == 0x15 then
-            if (gearswap.cued_packet or midaction()) and fixed_pos ~= '' and os.time()-fixed_ts < 5 then
-                return original:sub(1,4)..fixed_pos..original:sub(17)
-            else
-                fixed_pos = original:sub(5,16)
-                fixed_ts = os.time()
-            end
-        end
-    end
-end)
-
-register_unhandled_command(function (...)
-    local commands = {...}
-    if commands[1] and commands[1]:lower() == 'interrupts' then
-        if (no_interruptions) then
-            windower.add_to_chat(160, "%s : Disabling \30\2no_interruptions\30\43":format(_addon.name))
-            no_interruptions = false
-        else
-            windower.add_to_chat(160, "%s : Enabling \30\2no_interruptions\30\43":format(_addon.name))
-            no_interruptions = true
-        end
-        return true
-    end
-    return false
-end)
 
 function check_gear()
     if no_swap_gear:contains(player.equipment.left_ring) then
